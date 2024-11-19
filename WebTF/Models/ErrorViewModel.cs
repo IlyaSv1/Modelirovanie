05e354cf73556947ef4e335113287ab194eefbe7
Ilya <Ilya.sv7490@gmail.com>
2024-11-19 14:15:37 +0500

    Коммит № 1
    Создано приложение-калькулятор с помощью технологии ASP.NET MVC Core

---
+++ WebTF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Modelirovanie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
